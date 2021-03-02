f91968f73985fdf62dd594c337408ce0ed8a2433
nomoon456@gmail.com <nomoon456@gmail.com>
2021-03-02 18:20:57 +0100

    123

---
+++ ft_strdup.s
@@ -0,0 +1,20 @@
+extern malloc
+extern ft_strlen
+extern ft_strcpy
+
+section .text
+	global ft_strdup
+
+ft_strdup:
+	mov rdx, rdi
+	push rdx
+	call ft_strlen
+	mov rdi, rax
+	inc rdi
+	call malloc
+	pop rsi
+	mov rdi, rax
+	push rax
+	call ft_strcpy
+	pop rax
+	ret
---
+++ ft_write.s
@@ -0,0 +1,19 @@
+extern __errno_location
+
+section .text
+	global ft_write
+
+ft_write:
+	mov rax, 1
+	syscall
+	cmp rax, 0
+	jl error
+	ret
+
+error:
+	neg rax
+	push rax
+	call __errno_location
+	pop qword [rax]
+	mov rax, -1
+	ret
